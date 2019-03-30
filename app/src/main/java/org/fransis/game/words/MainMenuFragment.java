0cb8be3652744526b688afceec11000beb02ef60
Francisco Giana <gianafrancisco@gmail.com>
2019-03-30 15:02:39 -0300

    Google Game Service implementation.
    There are task pending for instance register applicationID to gogole service, 
    add Greeting messages.

--- app/src/main/java/org/fransis/game/words/MainMenuFragment.java
+++ app/src/main/java/org/fransis/game/words/MainMenuFragment.java
@@ -1,7 +1,6 @@
 package org.fransis.game.words;
 
 import android.content.Context;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -11,9 +10,12 @@ import android.widget.Button;
 
 import org.fransis.game.words.words.R;
 
-public class MainMenuFragment extends Fragment {
+public class MainMenuFragment extends Fragment implements View.OnClickListener {
 
     private OnFragmentInteractionListener mListener;
+    private View mSignInBarView;
+    private View mSignOutBarView;
+    private boolean mShowSignInButton = true;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -27,31 +29,18 @@ public class MainMenuFragment extends Fragment {
         View inflate = inflater.inflate(R.layout.fragment_main_menu, container, false);
 
         Button play = inflate.findViewById(R.id.btn_play);
+        play.setOnClickListener(this);
         Button exit = inflate.findViewById(R.id.btn_exit);
+        exit.setOnClickListener(this);
         Button resume = inflate.findViewById(R.id.btn_resume);
-        play.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if(mListener != null)
-                    mListener.play();
-            }
-        });
-        exit.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if(mListener != null)
-                    mListener.exit();
-            }
-        });
+        resume.setOnClickListener(this);
 
-        resume.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if(mListener != null)
-                    mListener.resume();
-            }
-        });
+        mSignInBarView = inflate.findViewById(R.id.sign_in_bar);
+        mSignInBarView.setOnClickListener(this);
+        mSignOutBarView = inflate.findViewById(R.id.sign_out_bar);
+        mSignOutBarView.setOnClickListener(this);
 
+        updateUI();
         return inflate;
     }
 
@@ -72,9 +61,42 @@ public class MainMenuFragment extends Fragment {
         mListener = null;
     }
 
+    private void updateUI() {
+        mSignInBarView.setVisibility(mShowSignInButton ? View.VISIBLE : View.GONE);
+        mSignOutBarView.setVisibility(mShowSignInButton ? View.GONE : View.VISIBLE);
+    }
+
+    public void setShowSignInButton(boolean showSignInButton) {
+        mShowSignInButton = showSignInButton;
+        updateUI();
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn_play:
+                mListener.onPlayButtonClicked();
+                break;
+            case R.id.btn_exit:
+                mListener.onExitButtonClicked();
+                break;
+            case R.id.btn_resume:
+                mListener.onResumeButtonClicked();
+                break;
+            case R.id.sign_in_bar:
+                mListener.onSignInButtonClicked();
+                break;
+            case R.id.sign_out_bar:
+                mListener.onSignOutButtonClicked();
+                break;
+        }
+    }
+
     public interface OnFragmentInteractionListener {
-        void play();
-        void exit();
-        void resume();
+        void onPlayButtonClicked();
+        void onExitButtonClicked();
+        void onResumeButtonClicked();
+        void onSignInButtonClicked();
+        void onSignOutButtonClicked();
     }
 }
